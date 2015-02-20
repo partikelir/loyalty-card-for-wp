c7ba36e14b35e87aa46d2e29bdc06655c8e3a5fa
Nicola Mustone <mustone.nicola@gmail.com>
2015-02-20 09:03:29 +0100

    removed admin footer text

--- includes/admin/class-lcwp-admin.php
+++ includes/admin/class-lcwp-admin.php
@@ -20,7 +20,7 @@ class LCWP_Admin {
 	public function __construct() {
 		add_action( 'init', array( $this, 'includes' ) );
 		add_action( 'admin_footer', 'lcwp_print_js', 25 );
-		add_filter( 'admin_footer_text', array( $this, 'admin_footer_text' ), 1 );
+		//add_filter( 'admin_footer_text', array( $this, 'admin_footer_text' ), 1 );
 	}
 
 	/**
@@ -34,26 +34,6 @@ class LCWP_Admin {
 		// Classes
 		include_once( 'class-lcwp-admin-post-type.php' );
 	}
-
-	/**
-	 * Change the admin footer text on WooCommerce admin pages
-	 *
-	 * @since  2.3
-	 * @param  string $footer_text
-	 * @return string
-	 */
-	public function admin_footer_text( $footer_text ) {
-		$current_screen = get_current_screen();
-
-		// Check to make sure we're on a WooCommerce admin page
-		if ( isset( $current_screen->id ) && apply_filters( 'woocommerce_display_admin_footer_text', in_array( $current_screen->id, wc_get_screen_ids() ) ) ) {
-			// Change the footer text
-			$footer_text = sprintf( __( 'If you like <strong>Loyalty Card for WordPress</strong> please leave us a <a href="%1$s" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a> rating on <a href="%1$s" target="_blank">WordPress.org</a>. A huge thank you from Nicola in advance!', 'woocommerce' ), 'https://wordpress.org/support/view/plugin-reviews/woocommerce?filter=5#postform' );
-		}
-
-		return $footer_text;
-	}
-
 }
 
 return new LCWP_Admin();
